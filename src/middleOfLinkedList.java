c506559f4924349b4ac49ff70c9026c2632506e2
Shivam <shivamchauhan121999@gmail.com>
2022-05-03 21:01:17 +0530

    Added Middle Of LinkedList Problem

---
+++ src/middleOfLinkedList.java
@@ -0,0 +1,22 @@
+public class middleOfLinkedList {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    // Time Complexity :- O(N)
+    // Space Complexity :- O(1)
+    public ListNode middleNode(ListNode head) {
+        ListNode fastPointer=head,slowPointer=head;
+        while(fastPointer!=null && fastPointer.next!=null){
+            fastPointer=fastPointer.next.next;
+            slowPointer=slowPointer.next;
+        }
+        return slowPointer;
+    }
+}
