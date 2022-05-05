d269163f2f06ddf60ee7a332b7583fcf24e2ca54
Shivam <shivamchauhan121999@gmail.com>
2022-05-05 21:42:47 +0530

    Added Delete a Node Linked List Problem

---
+++ src/deleteANodeLinkedList.java
@@ -0,0 +1,19 @@
+public class deleteANodeLinkedList {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+    // Time Complexity :- O(1)
+    // Space Complexity :- O(1)
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+
+    }
+}
+
+
