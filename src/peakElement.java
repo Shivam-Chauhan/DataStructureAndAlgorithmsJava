2676bf6eb2a21254c9115020b64a78c39f0dccb7
Shivam <shivamchauhan121999@gmail.com>
2022-07-15 14:09:34 +0530

    Added Peak Element Problem Code

---
+++ src/peakElement.java
@@ -0,0 +1,13 @@
+public class peakElement {
+    // Time Complexity :- ~ O(Log(N))
+    // Space Complexity :- O(1)
+    public int peakIndexInMountainArray(int[] arr) {
+        int low=0,high=arr.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]<arr[mid+1]) low=mid+1;
+            else high=mid-1;
+        }
+        return low;
+    }
+}
