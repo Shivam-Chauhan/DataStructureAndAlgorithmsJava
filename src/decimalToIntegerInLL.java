fb0a91263c0d0f48c5655d3e8a488bf66e52a2f5
Shivam <shivamchauhan121999@gmail.com>
2022-07-24 13:12:36 +0530

    Convert Integer Number in LL to Decimal Problem Code

---
+++ src/decimalToIntegerInLL.java
@@ -0,0 +1,13 @@
+public class decimalToIntegerInLL {
+    // Time Complexity :- O(N)
+    // Space Complexity :- O(1)
+    public int getDecimalValue(ListNode head) {
+        StringBuilder str=new StringBuilder();
+        ListNode tempHead=head;
+        while(tempHead!=null){
+            str.append(String.valueOf(tempHead.val));
+            tempHead=tempHead.next;
+        }
+        return Integer.parseInt(str.toString(),2);
+    }
+}
