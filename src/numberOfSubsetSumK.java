da45371d3f7075b30cb5004534c439c189f21bb8
Shivam <shivamchauhan121999@gmail.com>
2022-07-28 16:29:23 +0530

    Added Number Of SubSet Sum K Problem Code

--- src/numberOfSubsetSumK.java
+++ src/numberOfSubsetSumK.java
@@ -3,9 +3,9 @@ public class numberOfSubsetSumK {
     // Space Complexity :- O(N*M) + Auxillary Space Of Recursion Stack
     // link :- https://www.codingninjas.com/codestudio/problems/number-of-subsets_3952532?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos&leftPanelTab=0
     public static int subSetSumFinder(int[] arr, int target, int index,int[][] grid){
-        if(target==0) return 1;
         if(index==0){
-            if (arr[0]==target) return 1;
+            if(target==0 && arr[0]==0) return 2;
+            if(target==0 || target==arr[0]) return 1;
             return 0;
         }
         if(grid[index][target]!=-1) return grid[index][target];
