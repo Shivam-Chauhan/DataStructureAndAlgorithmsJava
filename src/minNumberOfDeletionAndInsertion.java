854fec8b02d66f47ee9b33baebd617e467ee271f
Shivam <shivamchauhan121999@gmail.com>
2022-07-30 13:18:10 +0530

    Added Min Number Of Deletion And Insertion Problem Code

---
+++ src/minNumberOfDeletionAndInsertion.java
@@ -0,0 +1,22 @@
+public class minNumberOfDeletionAndInsertion {
+    public static int lcsFinder(String s, String t, int index1, int index2, int[][] grid) {
+        if (index1 < 0 || index2 < 0) return 0;
+        if (grid[index1][index2] != -1) return grid[index1][index2];
+        if (s.charAt(index1) == t.charAt(index2)) {
+            return grid[index1][index2] = 1 + lcsFinder(s, t, index1 - 1, index2 - 1, grid);
+        }
+        return grid[index1][index2] = Math.max(lcsFinder(s, t, index1 - 1, index2, grid), lcsFinder(s, t, index1, index2 - 1, grid));
+    }
+
+    // Time Complexity :- O(N*M)
+    // Space Complexity :- O(N*M) + Auxillary Space Of Recursion Stack
+    // link :- https://www.codingninjas.com/codestudio/problems/can-you-make_4244510?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos&leftPanelTab=0
+    public static int canYouMake(String s, String t) {
+        int[][] grid = new int[s.length()][t.length()];
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j < t.length(); j++) grid[i][j] = -1;
+        }
+        int lengthOfLCS=lcsFinder(s, t, s.length() - 1, t.length() - 1, grid);
+        return (s.length()-lengthOfLCS)+(t.length()-lengthOfLCS);
+    }
+}
