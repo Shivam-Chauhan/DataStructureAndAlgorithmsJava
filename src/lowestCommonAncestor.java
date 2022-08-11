0c0021c18fbaf11b9efb4199ac4b07c7dca72a11
Shivam <shivamchauhan121999@gmail.com>
2022-08-11 20:48:31 +0530

    Added Lowest Common Ancestor Of Binary Tree Problem Code

--- src/lowestCommonAncestor.java
+++ src/lowestCommonAncestor.java
@@ -3,16 +3,12 @@ public class lowestCommonAncestor {
     // Space Complexity :- O(1) + Auxillary Space Of Recursion Stack
     // link :- https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/submissions/
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root.val==p.val || root.val==q.val) return root;
-        if(root.left==null && root.right==null) return null;
-        TreeNode left=null, right=null;
-        if(root.left!=null){
-            left=lowestCommonAncestor(root.left,p,q);
-        }
-        if(root.right!=null){
-            right=lowestCommonAncestor(root.right,p,q);
-        }
-        if(left!=null && right!=null) return root;
-        return left!=null?left:right;
+        if (root == null || root == p || root == q) return root;
+        TreeNode leftRoot = lowestCommonAncestor(root.left, p, q);
+        TreeNode rightRoot = lowestCommonAncestor(root.right, p, q);
+        if (leftRoot == null) return rightRoot;
+        else if (rightRoot == null) return leftRoot;
+        else return root;
     }
 }
+
