bc78970e6be582e5fe23f00b573b302c7e24e888
Shivam <shivamchauhan121999@gmail.com>
2022-08-11 17:36:28 +0530

    Added Iterative Inorder Traversal Problem Code

---
+++ src/inorderTraversalIterative.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class inorderTraversalIterative {
+    // Time Complexity :- O(N)
+    // Space Complexity :- O(N)
+    // Link :- https://leetcode.com/problems/binary-tree-inorder-traversal/
+    public List<Integer> inorderTraversal(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        List<Integer> output = new ArrayList<>();
+        TreeNode node = root;
+        while (true) {
+            if (node != null) {
+                stack.add(node);
+                node = node.left;
+            } else {
+                if (stack.isEmpty() == true) break;
+                node = stack.pop();
+                output.add(node.val);
+                node = node.right;
+
+            }
+
+        }
+        return output;
+    }
+}
+g
