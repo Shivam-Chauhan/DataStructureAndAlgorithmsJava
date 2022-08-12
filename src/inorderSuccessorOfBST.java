90c5b52ba4f4bd063c930fbfcbe16f0c9e9cb64f
Shivam <shivamchauhan121999@gmail.com>
2022-08-12 17:34:25 +0530

    Added Inorder Successor Of BST Problem Code

---
+++ src/inorderSuccessorOfBST.java
@@ -0,0 +1,17 @@
+public class inorderSuccessorOfBST {
+    // Time Complexity :- O(Log(N))
+    // Space Complexity :- O(1) + Auxillary Space Of Recursion Stack
+    // link :- https://practice.geeksforgeeks.org/problems/inorder-successor-in-bst/1
+    public Node inorderSuccessor(Node root, Node x) {
+        Node ans = null;
+        while (root != null) {
+            if (root.data > x.data) {
+                ans = root;
+                root = root.left;
+            } else {
+                root = root.right;
+            }
+        }
+        return ans;
+    }
+}
