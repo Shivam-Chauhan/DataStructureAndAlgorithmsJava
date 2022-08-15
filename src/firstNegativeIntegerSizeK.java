8dc1be62f48292fb85ab9882249b56795fedb530
Shivam <shivamchauhan121999@gmail.com>
2022-08-15 16:54:52 +0530

    Added First Negative Integer Window Size K Problem Code

--- src/firstNegativeIntegerSizeK.java
+++ src/firstNegativeIntegerSizeK.java
@@ -2,9 +2,8 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class firstNegativeIntegerSizeK {
-    // Time Complexity :- O(Log(N))
-    // Space Complexity :- O(1) + Auxillary Space Of Recursion Stack
-    // link :- https://practice.geeksforgeeks.org/problems/inorder-successor-in-bst/1
+    // Time Complexity :- O(N)
+    // Space Complexity :- O(N)
     public long[] printFirstNegativeInteger(long array[], int N, int K)
     {
         long[] output=new long[N-K+1];
