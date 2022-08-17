7ad67b1dbb6b9711fce5a8f0f8c5ab7fc7dd7aa3
Shivam <shivamchauhan121999@gmail.com>
2022-08-17 22:06:29 +0530

    Added Reverse Pair Two Pointer Problem Code

--- src/reversePairs.java
+++ src/reversePairs.java
@@ -1,2 +1,46 @@
+import java.util.ArrayList;
+
 public class reversePairs {
+    // Time Complexity :- O(N*Log(N)) + O(N) + O(N)
+    // Space Complexity :- O(N)
+    // Link :- https://leetcode.com/problems/reverse-pairs/
+    public int mergeArray(int[] nums, int low, int mid, int high) {
+        int counter = 0, j = mid + 1;
+        for (int i = low; i <= mid; i++) {
+            while (j <= high && nums[i] > (2 * (long) nums[j])) j++;
+            counter += j - (mid + 1);
+        }
+        ArrayList<Integer> temp = new ArrayList<>();
+        int left = low, right = mid + 1;
+        while (left <= mid && right <= high) {
+            if (nums[left] <= nums[right]) {
+                temp.add(nums[left++]);
+            } else {
+                temp.add(nums[right++]);
+            }
+        }
+        while (right <= high)
+            temp.add(nums[right++]);
+        while (left <= mid)
+            temp.add(nums[left++]);
+        for (int i = low; i <= high; i++) nums[i] = temp.get(i - low);
+        return counter;
+
+
+    }
+
+    public int mergeSort(int[] nums, int low, int high) {
+        if (low >= high) return 0;
+        int mid = (low + high) / 2;
+        int inversion = mergeSort(nums, low, mid);
+        inversion += mergeSort(nums, mid + 1, high);
+        inversion += mergeArray(nums, low, mid, high);
+
+        return inversion;
+
+    }
+
+    public int reversePairs(int[] nums) {
+        return mergeSort(nums, 0, nums.length - 1);
+    }
 }
