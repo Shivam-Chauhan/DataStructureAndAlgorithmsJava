0e8b7cdc42168a4312b1e73fb89a96ac8af336d9
Shivam <shivamchauhan121999@gmail.com>
2022-08-20 20:16:59 +0530

    Added Search Insert Position Binary Search Problem Code

---
+++ src/searchInsertPosition.java
@@ -0,0 +1,16 @@
+public class searchInsertPosition {
+    // Time Complexity :- O(Log(N))
+    // Space Complexity :- O(1)
+    // Link :- https://leetcode.com/problems/search-insert-position/
+    public int searchInsert(int[] nums, int target) {
+        int low=0,high=nums.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]<target) low=mid+1;
+            else high=mid-1;
+        }
+        return low;
+
+    }
+}
